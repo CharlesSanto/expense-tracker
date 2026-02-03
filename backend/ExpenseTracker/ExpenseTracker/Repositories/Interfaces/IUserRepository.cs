089e879d4ab67731f68a8788798071f77769dc71
Charles Santos <coliveirasantos1234@gmail.com>
2026-02-03 07:02:32 -0300

    Implementação inicial de repositórios, serviços e controllers
    Adicionadas interfaces e implementações básicas (NotImplementedException) para User e Transaction, incluindo métodos CRUD assíncronos. Criados UserController e TransactionController. Ajustado .csproj para remover referências a pastas de interfaces. Injeção de dependência aplicada nos serviços e repositórios.

---
+++ backend/ExpenseTracker/ExpenseTracker/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseTracker.Data.Models;
+
+namespace ExpenseTracker.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserByIdAsync(int userId);
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<User?> CreateUserAsync(User user);
+        Task<User?> UpdateUserAsync(User user);
+        Task<bool> DeleteUserAsync(int userId);
+    }
+}
---
+++ backend/ExpenseTracker/ExpenseTracker/Services/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using ExpenseTracker.Data.Models;
+
+namespace ExpenseTracker.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<User?> GetUserByIdAsync(int userId);
+        Task<User?> GetUserByEmailAsync(string email);
+        Task<User?> CreateUserAsync(User user);
+        Task<User?> UpdateUserAsync(User user);
+        Task<bool> DeleteUserAsync(int userId);
+    }
+}
